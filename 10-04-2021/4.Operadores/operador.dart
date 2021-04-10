cf1c3c20f5a87e4064af2db7febd5262c752e44f
Fabricio <fabricio@pop-os.localdomain>
2021-04-10 14:59:01 -0500

    Ejercicio de operadores

---
+++ 10-04-2021/4.Operadores/operador.dart
@@ -0,0 +1,10 @@
+void main(){
+    var isPublic = false;
+    var visible = !isPublic ? 'Privado' : 'Public';
+
+    var texto;
+    var prueba = texto ?? 'Porner texto';
+
+    print(visible);
+    print(prueba);
+}
